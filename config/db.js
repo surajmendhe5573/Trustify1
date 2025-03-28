9ef7e1e7f2a271defbb74011327192fc4993bba1
surajmendhe5573 <surajmendhe470@gmail.com>
2025-03-28 07:42:33 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 const mongoose= require('mongoose');
 
 const connectDB= ()=>{
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
         console.log('Database is connected');
-    }).catch((err)=>{
+    })
+    .catch((err)=>{
         console.log(err);
     })
 }
